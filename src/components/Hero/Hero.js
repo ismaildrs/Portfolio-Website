4efc85f4381d92fdb3d03530ce8a9c7478be07be
Ismail Drissi <drissiisismail@gmail.com>
2024-04-19 23:10:15 +0000

    Fourth commit

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -22,7 +22,7 @@ export default function Hero() {
         <div className={styles.btn_section}>
           <button><a href='https://www.linkedin.com/in/ismaildrs/'><FaLinkedin className={styles.icons}/><span>Linkedin</span></a></button>
           <button><a href='https://github.com/ismaildrs'><FaGithub className={styles.icons}/><span>Github</span></a></button>
-          <button><a ><IoMdMail className={styles.icons}/><span>drissiisismail@gmail.com</span></a></button>
+          <button><IoMdMail className={styles.icons}/><span>drissiisismail@gmail.com</span></button>
           <button><a href='../../assets/' download='ismail_drissi_cv.pdf'><RiPagesFill className={styles.icons}></RiPagesFill><span>CV</span></a></button>
         </div>
       </div>
