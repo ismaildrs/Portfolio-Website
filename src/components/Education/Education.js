d1d310160a238cb766063bcc2689ca141cf7b306
Ismail Drissi <drissiisismail@gmail.com>
2024-04-20 10:33:05 +0000

    commit by ismail

--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -11,7 +11,7 @@ export default function Education() {
         <div className={styles.design}>
         </div>
         <div className={styles.section_items}>
-          <Item time="Sep 2023 - Today" desc="Software and Distributed Systems Engineering" more_infos="ENSET Mohammmedia"/>
+          <Item time="Sep 2023 - Today" desc="Software and Distributed Computer Systems Engineering" more_infos="ENSET Mohammmedia"/>
           <Item time="Sep 2021 - Jul 2023" desc="DEUST" more_infos="FST Fès"/>
         </div>
       </div>
